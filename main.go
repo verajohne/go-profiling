fc4f85b3c8c3d8b097efbe8ba08f226c1585c2cf
Ben Fritsch <beanie@benle.de>
2020-08-10 12:54:30 +0200

    read and write from redis

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-  "net/http"
+	"context"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
+	ctx := context.Background()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	r.Get("/data", func(w http.ResponseWriter, r *http.Request) {
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+		val, err := rdb.Get(ctx, "jsonData").Result()
+		if err != nil {
+			panic(err)
+		}
+
+		w.Write([]byte(val))
+	})
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome\n"))
-    resp, _ := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+		resp, _ := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+		body, _ := ioutil.ReadAll(resp.Body)
+		err := rdb.Set(ctx, "jsonData", body, 0).Err()
+		if err != nil {
+			panic(err)
+		}
+
+		w.Write([]byte("Written to Redis"))
 	})
 	http.ListenAndServe(":3000", r)
 }
