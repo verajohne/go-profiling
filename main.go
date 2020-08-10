3ada4a17658b65fed4e6319111d7e9b36ef45ad7
Vera Johne <vera@gangeskar.com>
2020-08-10 12:25:38 +0200

    start

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome\n"))
+    resp, _ := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	})
+	http.ListenAndServe(":3000", r)
+}
